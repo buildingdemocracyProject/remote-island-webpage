2be41395e975e574a5ee03c5fbe39d00b5fbc61b
Pat173 <juan.ortiz1734@gmail.com>
2024-11-19 15:57:44 +0100

    test 3

--- Build/remote-island-webpage.framework.js
+++ Build/remote-island-webpage.framework.js
@@ -15966,6 +15966,7 @@ var ASM_CONSTS = {
                       Module.SendMessage("ExternalTools", "OnDataLoadedCallback", jsonData);
   
                       // Load the next item
+                      console.debug("data sent on index" + indexVal + jsonData);
                       indexVal++;
                       loadNext();
                   }
